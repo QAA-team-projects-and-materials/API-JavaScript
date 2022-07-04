ebd5a170aab61a6cbd563ca62bfff4441f30e608
Daria <dasha.tolstaya18@gmail.com>
2022-07-04 17:13:11 +0300

    Add negative tests

--- fixtures/users_helper.js
+++ fixtures/users_helper.js
@@ -1,19 +1,22 @@
 import supertest from "supertest";
+import { faker } from "@faker-js/faker";
 
 const request = supertest("https://gorest.co.in/public-api");
 const TOKEN = 
     "6fc03d210d2afad24cbcee1c75547f463a55537e9bb95ab3670966ed86a2fe5a";
 
-export const createRandomUser = async () => {
+export const createRandomUserWithFaker = async () => {
     const userData = {
-        email:`Test-${Math.floor(Math.random()* 9999)}@gmail.com`,
-        name: 'Test name',
+        email: faker.internet.email(),
+        name: faker.name.firstName(),
         gender:"female",
         status: 'inactive',
     };
 
     const res = await request
         .post(`/users?access-token=${TOKEN}`)
-        .send(userData)
+        .send(userData);
+
+        console.log(res.body);
     return res.body.data.id;
 };
